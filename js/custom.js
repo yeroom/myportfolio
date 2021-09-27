db435fa725e60c4ccd0b5d551b3edcea2d0f632a
yeah210 <pek3855@naver.com>
2021-09-27 14:58:55 +0900

    수정

--- js/custom.js
+++ js/custom.js
@@ -140,6 +140,105 @@ $(document).ready(function () {
         modalTarget: 'animatedModal-10'
     });
 
+     // 스크롤시 숫자
+     var a = 0;
+     $(window).scroll(function() {
+     
+       var oTop = $('#counter').offset().top - window.innerHeight;
+       if (a == 0 && $(window).scrollTop() > oTop) {
+         $('.counter-value').each(function() {
+           var $this = $(this),
+             countTo = $this.attr('data-count');
+           $({
+             countNum: $this.text()
+           }).animate({
+               countNum: countTo
+             },
+             {
+               duration: 2000,
+               easing: 'swing',
+               step: function() {
+                 $this.text(Math.floor(this.countNum));
+               },
+               complete: function() {
+                 $this.text(this.countNum);
+               }
+             });
+         });
+         a = 1;
+       }
+     });
+
+    // 타이핑
+    var TxtRotate = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+    };
+
+    TxtRotate.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function () {
+    that.tick();
+    }, delta);
+    };
+
+    window.onload = function () {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+    new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+    };
+
+
+
+
+    //스크롤 닿으면 show
+    $(window).scroll(function(){
+        var location = $(window).scrollTop(); //스크롤의 현재 위치
+        
+        if(location >= $("#experience").offset().top){
+            $(".graph").show();
+        }
+
+        });
 
 
     // Contact Form 	
